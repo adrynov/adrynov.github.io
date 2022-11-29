79c98104b94ba241f7757ce0d55bd415fb810ada
Andrei Drynov <adrynov@pm.me>
2022-11-29 17:57:07 +0000

    Let's break HTML into components

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer.component';
+import { HeaderComponent } from './components/header.component';
+import { HeroComponent } from './components/hero.component';
 import { MainComponent } from './components/main.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, FooterComponent],
+  declarations: [AppComponent, MainComponent, FooterComponent, HeaderComponent, HeroComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ Portfolio/src/app/components/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'afd-hero',
+  templateUrl: 'hero.component.html',
+})
+export class HeroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
