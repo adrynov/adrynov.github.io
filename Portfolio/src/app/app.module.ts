34c09fafdbdd2ce452ee3679d2b2487a9df61eda
Andrei Drynov <adrynov@pm.me>
2022-11-29 17:22:54 +0000

    Start converting portfolio website to Angular

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer.component';
+import { MainComponent } from './components/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
