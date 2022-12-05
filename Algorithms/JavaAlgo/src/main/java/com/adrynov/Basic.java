6c7549f164ed56d6a7268235e14422dfc25e83fe
Andrei Drynov <adrynov@pm.me>
2022-12-05 10:54:37 +0000

    Missed changes

--- Algorithms/JavaAlgo/src/main/java/com/adrynov/Basic.java
+++ Algorithms/JavaAlgo/src/main/java/com/adrynov/Basic.java
@@ -2,6 +2,22 @@ package com.adrynov;
 
 public class Basic {
 
+    /**
+     * Computes the nth Fibonacci number in the sequence (in which each number is the sum of the two preceding ones)
+     * <p>
+     * Successive Fibonacci numbers: 1, 1, 2, 3, 5, 8, 13 and 21.
+     *
+     * @param n The number of Fibonacci numbers to generate.
+     * @return Fibonacci sequence.
+     */
+    public static int fibonacci(int n) {
+        // base cases
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+
     public static int findMaximum(int a, int b, int c) {
         int max = a;
 
--- Algorithms/JavaAlgo/src/test/java/BasicTests.java
+++ Algorithms/JavaAlgo/src/test/java/BasicTests.java
@@ -6,6 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BasicTests {
 
     @Test
+    void fibonacci() {
+        // 1, 1, 2, 3, 5, 8, 13 and 21.
+        assertEquals(1, Basic.fibonacci(1));
+        assertEquals(1, Basic.fibonacci(2));
+
+        assertEquals(3, Basic.fibonacci(4));
+        assertEquals(5, Basic.fibonacci(5));
+        assertEquals(8, Basic.fibonacci(6));
+        assertEquals(13, Basic.fibonacci(7));
+        assertEquals(21, Basic.fibonacci(8));
+        assertEquals(34, Basic.fibonacci(9));
+        assertEquals(55, Basic.fibonacci(10));
+        assertEquals(89, Basic.fibonacci(11));
+        assertEquals(144, Basic.fibonacci(12));
+    }
+
+    @Test
     void findMaximumNumber() {
         assertEquals(3, Basic.findMaximum(1, 2, 3));
         assertEquals(8, Basic.findMaximum(8, 8, 1));
