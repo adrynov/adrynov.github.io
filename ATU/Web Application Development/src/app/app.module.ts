4f317601bbad181a691847d752a11a523a600a0e
Andrei Drynov <adrynov@pm.me>
2023-01-28 18:46:06 +0000

    Angular components

--- ATU/Web Application Development/src/app/app.module.ts
+++ ATU/Web Application Development/src/app/app.module.ts
@@ -9,13 +9,22 @@ import { MainComponent } from './main/main.component';
 import { PortfolioItemComponent } from './portfolio/porfolio-item.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
-import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FaqComponent } from './components/faq/faq.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { PricingComponent } from './pricing/pricing.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { WhyusComponent } from './whyus/whyus.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TeamComponent } from './team/team.component';
+import { ServicesComponent } from './services/services.component';
+import { CtaComponent } from './cta/cta.component';
+
 
 const APP_COMPONENTS = [
-  HeroComponent, ContactComponent, FooterComponent
+  ClientsComponent, ContactComponent, FaqComponent, FooterComponent, HeroComponent, PricingComponent, AboutComponent, WhyusComponent
 ];
 
 @NgModule({
@@ -25,10 +34,13 @@ const APP_COMPONENTS = [
     PortfolioComponent,
     PortfolioItemComponent,
     ...APP_COMPONENTS,
-    PricingComponent
+    SkillsComponent,
+    TeamComponent,
+    ServicesComponent,
+    CtaComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ ATU/Web Application Development/src/app/whyus/whyus.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'afd-whyus',
+  templateUrl: './whyus.component.html',
+  styleUrls: ['./whyus.component.scss']
+})
+export class WhyusComponent {
+
+}
